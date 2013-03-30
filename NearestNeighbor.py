aeecb36d7a430da1f3ce96e4efe84453be97ed3f
Michael Hadley <mikewesthad@gmail.com>
2013-03-30 12:51:54 -0700

    Function for nearest neighbor placement of cells
    I tested a couple of algorithms for placing cells, but the one that was
    the fastest was the one in which I randomly generate points until valid
    locations are found.

---
+++ NearestNeighbor.py
@@ -0,0 +1,126 @@
+import random
+import math
+
+
+"""
+Small Retina class to keep hold of the dimensions in real world space and in grid space
+"""
+class Retina:
+    def __init__(self, retinaWidth, retinaHeight, gridSize):
+        self.width  = float(retinaWidth)
+        self.height = float(retinaHeight)
+        self.area   = retinaWidth * retinaHeight
+        
+        self.gridSize = float(gridSize)
+        
+        self.gridWidth  = int(self.width / self.gridSize)
+        self.gridHeight = int(self.height / self.gridSize)
+
+
+
+"""
+Linear distance between two points
+"""
+def linearDistance(x1, y1, x2, y2):
+    return ((x2-x1)**2.0 + (y2-y1)**2.0)**0.5
+
+
+
+"""
+Nearest neighbor distance constrained placement of points
+    The method involves tracking point locations and tracking exclusion zones around those points
+    Random points are generated until:
+        A valid point is found (one that is within the bounds and not located within an exclusion zone
+        A maximum number of tries has been exhausted
+        
+    Note: this isn't the most accurate algorithm, but it is *MUCH* faster than the other algorithms I tried
+"""
+def nearestNeighborRandom(retina, minDistance, density, densityArea, maxRandTries=1000):
+
+    # Set the bounds on the positions
+    xmin = 0
+    ymin = 0
+    xmax = retina.gridWidth - 1
+    ymax = retina.gridHeight - 1
+
+    # Convert the minimum distance from world units to grid units
+    griddedDistance = minDistance / retina.gridSize
+    ceilGriddedDistance = int(math.ceil(griddedDistance))
+
+    # Calculate the number of cells to place
+    requiredNumberCells = int(density * (retina.area/densityArea))
+    currentNumberCells  = 0
+
+    # Create empty sets to hold the selected positions and the excluded positions
+    positions           = set()
+    excludedPositions   = set()
+
+    while currentNumberCells < requiredNumberCells:
+
+        # Pick a random point
+        x       = random.randint(xmin, xmax)
+        y       = random.randint(ymin, ymax)
+        locID   = str(x) + "." + str(y)
+
+        # Regenerate random point until a valid point is found
+        randTries = 0
+        while locID in excludedPositions:
+            x = random.randint(xmin, xmax)
+            y = random.randint(ymin, ymax)
+            locID = str(x) + "." + str(y)
+            
+            randTries += 1
+            if randTries > maxRandTries: break
+
+        # If too many attempts were made to generate a new point, exit loop
+        if randTries > maxRandTries: break    
+
+        # Update the sets with the newly selected point
+        excludedPositions.add(locID)
+        positions.add(locID) 
+
+        # Find the bounding box of excluded coordinates surrounding the new point
+        left            = max(x - ceilGriddedDistance, xmin)
+        right           = min(x + ceilGriddedDistance, xmax)
+        up              = max(y - ceilGriddedDistance, ymin)
+        down            = min(y + ceilGriddedDistance, ymax)
+
+        # Check if each point in the bounding box is within the minimum distance radius
+        # If so, add it to the exclusion set
+        for x2 in range(left, right+1):
+            for y2 in range(up, down+1):
+                if linearDistance(x, y, x2, y2) < griddedDistance:
+                    locID = str(x2) + "." + str(y2)
+                    excludedPositions.add(locID)
+
+        currentNumberCells += 1
+
+    return currentNumberCells, positions
+
+
+    
+
+
+um_to_m = 1/1000000.0
+m_to_um = 1/1000000.0
+
+retinaWidth     = 1000 *  um_to_m
+retinaHeight    = 1000 *  um_to_m
+retinaGridSize  = 1 * um_to_m
+retina          = Retina(retinaWidth, retinaHeight, retinaGridSize)
+
+
+minDistance     = 10 * um_to_m
+density         = 10000.0
+densityArea     = retinaWidth * retinaHeight
+numberCells,ps  = nearestNeighborRandom(retina, minDistance, density, densityArea, maxRandTries=10000)
+
+print numberCells
+
+##import cProfile
+##cProfile.run("print nearestNeighborRandom(retina, minDistance, density, densityArea, maxRandTries=100)")
+##cProfile.run("print nearestNeighborRandom(retina, minDistance, density, densityArea, maxRandTries=1000)")
+##cProfile.run("print nearestNeighborRandom(retina, minDistance, density, densityArea, maxRandTries=10000)")
+
+
+
